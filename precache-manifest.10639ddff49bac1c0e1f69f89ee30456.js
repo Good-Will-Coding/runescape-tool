291ea45bee6b84ff956a97dadb0d94b6b31149b5
billy <billyliadphillips@gmail.com>
2018-12-12 14:36:16 +0000

    Updates

---
+++ precache-manifest.10639ddff49bac1c0e1f69f89ee30456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fbff49cc1707ee22e53",
+    "url": "/myapp/static/css/main.cb4283d9.chunk.css"
+  },
+  {
+    "revision": "8fbff49cc1707ee22e53",
+    "url": "/myapp/static/js/main.8fbff49c.chunk.js"
+  },
+  {
+    "revision": "4c2dc49a5258352886f6",
+    "url": "/myapp/static/css/1.fa8688de.chunk.css"
+  },
+  {
+    "revision": "4c2dc49a5258352886f6",
+    "url": "/myapp/static/js/1.4c2dc49a.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "655d21e59d74322fece8b5d81ae073b5",
+    "url": "/myapp/index.html"
+  }
+];
